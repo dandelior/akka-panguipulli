3c771c75f6351e46f910d4573358cb46395ca9ce
Juan Berrios <jberrioscifuentes@gmail.com>
2023-01-01 21:00:35 -0300

    update form

--- parts/Contact.js
+++ parts/Contact.js
@@ -1,16 +1,25 @@
-import styles from "../styles/parts/Contact.module.sass";
+import { useForm, ValidationError } from '@formspree/react';
+import styles from '../styles/parts/Contact.module.sass';
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm('xbjejrad');
+  console.log(state);
+
   return (
     <div className={styles.contactSection} id="contact">
       <div className={`centeredContent ${styles.contactSectionInner}`}>
         <div className={styles.contactSectionHeader}>
           <h2>Contáctanos</h2>
         </div>
-        <div className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.inputs}>
-            <input type="text" placeholder="Nombre" />
-            <input type="email" placeholder="Correo" />
+            <input type="text" name="name" placeholder="Nombre" required />
+            <input type="email" name="email" placeholder="Correo" required />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
           <textarea
             name="message"
@@ -18,9 +27,33 @@ const Contact = () => {
             cols="30"
             rows="10"
             placeholder="Mensaje"
+            required
           ></textarea>
-          <button>Enviar Mensaje</button>
-        </div>
+          {state.submitting && (
+            <div className={styles.formMessage}>
+              <p>Enviando...</p>
+            </div>
+          )}
+          {state.succeeded && (
+            <div className={styles.formMessage}>
+              <p>
+                Gracias, tu mensaje ha sido enviado. Nos pondrémos en contacto
+                contigo a la brevedad.
+              </p>
+            </div>
+          )}
+          {state.errors.length !== 0 && (
+            <div className={`${styles.formMessage} ${styles.isErrorMessage}`}>
+              <p>
+                Ha ocurrido un error al enviar el mensaje, por favor intenta más
+                tarde.
+              </p>
+            </div>
+          )}
+          <button type="submit" disabled={state.submitting || state.succeeded}>
+            Enviar Mensaje
+          </button>
+        </form>
         <div className={styles.personalContact}>
           <div className={styles.item}>
             <p className={styles.strong}>Salazar Propiedades</p>
