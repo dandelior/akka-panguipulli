da7343f1353c79c53d9108252b8b663f5c6c8ab0
Juan Berrios <jberrioscifuentes@gmail.com>
2023-01-03 18:58:25 -0300

    add basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: '/akka-panguipulli',
+  assetPrefix: '/akka-panguipulli',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
